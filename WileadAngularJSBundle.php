acce5991fa39c95d84150750c85414ef0f0680ef
JackLam <jack@wizmacau.com>
2014-09-08 13:05:36 +0800

    symfony2 wrapper

---
+++ WileadAngularJSBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * WileadAngularJSBundle.php
+ * jack<jack@wizmacau.com>
+ * 2014-09-08
+ */
+
+namespace Wilead\AngularJSBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WileadAngularJSBundle extends Bundle
+{
+
+} 
